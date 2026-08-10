48daf307ff427643465ce48944001ffdbd88dfda
luanferreiraoliveira14-tech <luan.ferreira.oliveira14@escola.pr.gov.br>
2026-08-10 12:14:29 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,10 +1,12 @@
-a = [0,1]
-b = 0
-c = 10
-print(a[0])
-print(a[1])
+# #goden ratio
+# a = [0,1]
+# b = 0
+# #number of the sequence
+# c = 10
+# print(a[0])
+# print(a[1])
 
-while b < 10:
-    b += 1
-    a.append(a[-1] + a[-2])
-    print(a[-1])
+# while b < 10:
+#     b += 1
+#     a.append(a[-1] + a[-2])
+#     print(a[-1])
