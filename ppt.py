d869a1d9b0b208819d1cb15fc939ac31ef40fae1
lun4 <luan.ferreira.oliveira14@escola.pr.gov.br>
2026-09-23 17:47:38 -0300

    Delete ppt.py

--- ppt.py
+++
@@ -1,9 +0,0 @@
-import random
-
-escolhaC = ["pedra", "papel", "tesoura"]
-sorteio = random.choice(escolhaC)
-escolhaP = input("pedra papel tesoura")
-print(escolhaC)
-if escolhaC == escolhaP:
-    print("EMPATE")
-
